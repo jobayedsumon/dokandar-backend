afd5aacc84f5d5c48b74ee3e88fa66c9fda849ec
Jobayed Sumon <jobayedahmedsumon@gmail.com>
2021-07-03 23:18:37 +0600

    Nearby distance 0

--- app/Http/Controllers/Api/NearbystoreController.php
+++ app/Http/Controllers/Api/NearbystoreController.php
@@ -15,22 +15,29 @@ class NearbystoreController extends Controller
     	$lng = $request->lng;
     	$vendor_category_id = $request->vendor_category_id;
 
-
-        $groupApp = DB::table("vendor")
-     
-      ->select("vendor.vendor_name","vendor.vendor_phone","vendor.vendor_id","vendor.vendor_logo","vendor.vendor_category_id","vendor.lat","vendor.lng","vendor.delivery_range","vendor.online_status","vendor.about","vendor.vendor_loc"
-        ,DB::raw("6371 * acos(cos(radians(".$lat . ")) 
+    	if ($lat && $lng) {
+            $groupApp = DB::table("vendor")
+                ->select("vendor.vendor_name","vendor.vendor_phone","vendor.vendor_id","vendor.vendor_logo","vendor.vendor_category_id","vendor.lat","vendor.lng","vendor.delivery_range","vendor.online_status","vendor.about","vendor.vendor_loc"
+                    ,DB::raw("6371 * acos(cos(radians(".$lat . ")) 
         * cos(radians(lat)) 
         * cos(radians(lng) - radians(" . $lng . ")) 
         + sin(radians(" .$lat. ")) 
         * sin(radians(lat))) AS distance"))
-        ->orderBy('distance')
-        ->where('vendor_category_id',$vendor_category_id)
-        ->get();
+                ->orderBy('distance')
+                ->where('vendor_category_id',$vendor_category_id)
+                ->get();
+        } else {
+            $groupApp = DB::table("vendor")
+                ->select("vendor.vendor_name","vendor.vendor_phone","vendor.vendor_id","vendor.vendor_logo","vendor.vendor_category_id","vendor.lat","vendor.lng","vendor.delivery_range","vendor.online_status","vendor.about","vendor.vendor_loc")
+                ->where('vendor_category_id',$vendor_category_id)
+                ->get();
+        }
+
         $storelist = NULL;
         foreach($groupApp as $store)
         {
-            if($store->delivery_range > $store->distance){
+            $distance = $store->distance ?? 0;
+            if($store->delivery_range > $distance){
                 $storelist[] = $store; 
             }
         }
