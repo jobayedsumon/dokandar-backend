d70c46f385953014be2991dd5d94d9dc20ef29bb
JobayedSumon <jobayed.ahamed@apsissolutions.com>
2022-05-20 17:28:15 +0600

    fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // if (env('APP_ENV') === 'local') {
+         if (env('APP_ENV') === 'local') {
+            URL::forceScheme('http');
+         } else {
             URL::forceScheme('https');
-        // }
+         }
     }
 
     /**
